5d2d086a2466c45099cd1b3c763bb5a1fd804d40
dmathe <david.mathe@whis-consulting.com>
2021-09-24 02:53:32 +0200

    conf

--- next.config.js
+++ next.config.js
@@ -1,10 +1,27 @@
 const path = require('path')
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 
-module.exports = {
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
-    },
-    trailingSlash: true,
-    basePath: '/kln-debarras',
-    assetPrefix: '/kln-debarras/',
-}
+const config = 
+
+module.exports = withPlugins([
+    [optimizedImages, {
+      mozjpeg: {
+        quality: 80,
+      },
+      pngquant: {
+        speed: 3,
+        strip: true,
+        verbose: true,
+      },
+      imagesPublicPath: '/kln-debarras/_next/static/images/',
+    }],
+    {
+        sassOptions: {
+            includePaths: [path.join(__dirname, 'styles')],
+        },
+        trailingSlash: true,
+        basePath: '/kln-debarras',
+        assetPrefix: '/kln-debarras/',
+    }
+  ]);
