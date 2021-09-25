a043f021855757e5422ba88ca3f5bdb49834556a
dmathe <david.mathe@whis-consulting.com>
2021-09-25 19:18:32 +0200

    GTM

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,22 @@ class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
+                <script
+                    async
+                    src={`https://www.googletagmanager.com/gtag/js?id=G-QZB76L0E5R`}
+                />
+                <script
+                    dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-QZB76L0E5R', {
+                    page_path: window.location.pathname,
+                    });
+                `,
+                    }}
+                />
                     <link rel="icon" type="image/png" href="/images/favicon.ico"></link>
                 </Head>
                 <body>
