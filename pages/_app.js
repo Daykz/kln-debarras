17215ece67964056a124798f883e0323feb580d3
David MATHE <david.mathe@whis-consulting.com>
2022-12-19 06:40:44 +0100

    good seo

--- pages/_app.js
+++ pages/_app.js
@@ -8,16 +8,22 @@ import "react-sweet-progress/lib/style.css";
 import '../public/css/style.css';
 import '../public/css/responsive.css';
 
+import villesInfos from '../utils/villes';
 import App from 'next/app';
 import Head from 'next/head';
 
 export default class MyApp extends App {
     render () {
         const { Component, pageProps } = this.props
-const jsonLd = {
+  const ville = pageProps?.ville?.[0] || ''
+
+  const title = ville ? `Débarras et déménagement à ${villesInfos.find((info) => info.url === ville).titre} - KLN Débarras` : `Débarras et déménagement dans votre région Centre - KLN Débarras`
+  const canonical = `https://kln-debarras.fr/${ville}`
+
+  const jsonLd = {
     "@context": "https://schema.org",
     "@type": "LocalBusiness",
-    "name": "Débarras et déménagement dans votre région Centre - KLN Débarras",
+    "name": title,
     "image": [
         "https://kln-debarras.fr/images/1x1/principal2_1x1.jpg",
         "https://kln-debarras.fr/images/4x3/principal2_4x3.jpg",
@@ -36,7 +42,7 @@ const jsonLd = {
         "latitude": 47.036408,
         "longitude": 1.908068
       },
-    "url": "https://kln-debarras.fr",
+    "url": canonical,
     "telephone": "0769773262"
 }
         return (
@@ -44,12 +50,12 @@ const jsonLd = {
                 <Head>
                     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
                     <meta name="keywords" content="Debarras, demenagement, encombrants" />
-                    <link rel="canonical" href="https://kln-debarras.fr" />
+                    <link rel="canonical" href={canonical} />
                     
-                    <title>Débarras et déménagement dans votre région Centre - KLN Débarras</title>
+                    <title>{title}</title>
                     <meta
                     name="description"
-                    content="KLN débarras s'occupe de vos débarras, du tri des encombrants, et de les déposer en centre de tri. Devis gratuit."
+                    content={`KLN débarras s'occupe de vos débarras, du tri des encombrants, et de les déposer en centre de tri. ${villesInfos.find((info) => info.url === ville)?.link || 'Débarras Centre'}. Devis gratuit.`}
                     />
                     <script
                         type="application/ld+json"
